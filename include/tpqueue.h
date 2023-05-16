004debcd27e743b4cf0a59b8299c6e822138a5e5
lisyonkovaa <114025777+lisyonkovaa@users.noreply.github.com>
2023-05-16 08:29:02 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -2,14 +2,66 @@
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
 
-template<typename T, int size>
-class TPQueue {
-  // реализация шаблона очереди с приоритетом на кольцевом буфере
-};
+#include <cassert>
 
 struct SYM {
   char ch;
   int prior;
+};
+
+template<typename T, int size>
+class TPQueue {
+ private:
+  T* arr;
+  int sizeQueue;
+  int begin, end;
+  int count;
+
+ public:
+  TPQueue():sizeQueue(size), begin(0), end(0), count(0) {
+    arr = new T[sizeQueue + 1];
+  }
+
+  ~TPQueue() {
+    delete[] arr;
+  }
+
+  void push(const T& value) {
+    assert(count < sizeQueue);
+
+    count++;
+    int b = begin, e = end;
+    while (arr[b].prior >= value.prior && b != end)
+    b = (b + 1) % size;
+    while (e != b) {
+      arr[e] = arr[(e - 1) % size];
+      e = (size + e - 1) % size;
+    }
+    end = (end + 1) % size;
+    arr[b] = value;
+  }
+
+  char get() {
+    assert(count > 0);
+    return arr[begin].ch;
+  }
+
+  const T& pop() {
+    assert(count > 0);
+    count--;
+    if (begin > sizeQueue) {
+      begin -= sizeQueue + 1;
+    }
+    return arr[begin++];
+  }
+
+  bool isEmpty() const {
+    return count == 0;
+  }
+
+  bool isFull() const {
+    return count == sizeQueue;
+  }
 };
 
 #endif  // INCLUDE_TPQUEUE_H_
